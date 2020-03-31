3167bb2e4a120b3f5d0aa20cea8c2bd7a3e1353c
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-03-31 15:59:12 -0700

    eqArrays wokring fn
    Co-authored-by: Andy Lindsay <andydlindsay@gmail.com>

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😀😀😀 Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`👿👿👿 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+const eqArrays = function(array1,array2) {
+  // for (let i = 0; i < array1.length; i++) {
+  //   if (array1[i] !== array2[i]) {
+  //     return false;
+  //   }
+  // }
+  // return true;
+  return (JSON.stringify(array1) === JSON.stringify(array2));
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+// console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 4]),true));
+// console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true));
+// console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),true));
+
