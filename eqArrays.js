946a024c77bc7fdf1520e858c90adedbbe04bca4
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-03-31 16:11:00 -0700

    took out the unnecc.. assert

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😀😀😀 Assertion Passed: ${[actual]} === ${[expected]}`);
-  } else {
-    console.log(`👿👿👿 Assertion Failed: ${[actual]} !== ${[expected]}`);
-  }
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`😀😀😀 Assertion Passed: ${[actual]} === ${[expected]}`);
+//   } else {
+//     console.log(`👿👿👿 Assertion Failed: ${[actual]} !== ${[expected]}`);
+//   }
+// };
 
 const eqArrays = function(array1,array2) {
   return (JSON.stringify(array1) === JSON.stringify(array2));
