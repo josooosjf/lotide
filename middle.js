7307ee0c5c1248e6749ca5914e2c947048985e61
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-04-01 10:46:21 -0700

    middle finished

---
+++ middle.js
@@ -0,0 +1,13 @@
+let middle = function(array) {
+  let middleEven =  ((array.length) / 2 + 1);
+  let middleEven2 = ((array.length) / 2 + 2);
+  let middleOdd = Math.round(array.length / 2 + 1);
+
+  if (array.length <= 2) {
+    return [];
+  }
+ 
+  return array.length % 2 === 0 ? [middleEven,middleEven2] : [middleOdd];
+};
+
+console.log(middle([2,3]));
