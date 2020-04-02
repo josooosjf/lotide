2e4a7c857c8a8593eb046390efbeac09af9b3a3f
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-04-02 13:44:41 -0700

    fixed AssertArraysEquals

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,14 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
+const assertArraysEqual = function(actual, expected,) {
+  if (eqArrays(actual,expected)) {
     console.log(`😀😀😀 Assertion Passed: ${[actual]} === ${[expected]}`);
   } else {
     console.log(`👿👿👿 Assertion Failed: ${[actual]} !== ${[expected]}`);
   }
 };
 
+
 const eqArrays = function(array1,array2) {
   return (JSON.stringify(array1) === JSON.stringify(array2));
 };
 
-const assertArraysEqual = function(array1, array2,) {
-  return (assertEqual(eqArrays(array1, array2), true));
-};
-
-
-
-// console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 4]),false));
-// console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true));
-// console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false));
-
-console.log(assertArraysEqual([1,2,3],[1,2,4],false));
+assertArraysEqual([1,2,3],[1,2,3]);
