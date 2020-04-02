8a5aa13b357ddd1ab54031d6050b2062f5d8df98
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-04-02 14:47:52 -0700

    take until finished
    Co-authored-by: Hafiz Suara <hafbau@yahoo.com>

--- map.js
+++ map.js
@@ -1,3 +1,16 @@
+const assertArraysEqual = function(actual, expected,) {
+  if (eqArrays(actual,expected)) {
+    console.log(`😀😀😀 Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`👿👿👿 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+const eqArrays = function(array1,array2) {
+  return (JSON.stringify(array1) === JSON.stringify(array2));
+};
+
+
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array,callback) {
@@ -9,7 +22,8 @@ const map = function(array,callback) {
 };
 
 
-const results1 = map(words, word => word[0]);
+const results1 = map(words, word => word[0] + ' This is the first letter of');
+const results2 = map(words, word => word[1]);
 
 
-console.log(results1);
+assertArraysEqual(results2,results1);
---
+++ takeUntil.js
@@ -0,0 +1,38 @@
+const assertArraysEqual = function(actual, expected,) {
+  if (eqArrays(actual,expected)) {
+    console.log(`😀😀😀 Assertion Passed: ${[actual]} === ${[expected]}`);
+  } else {
+    console.log(`👿👿👿 Assertion Failed: ${[actual]} !== ${[expected]}`);
+  }
+};
+
+
+const eqArrays = function(array1,array2) {
+  return (JSON.stringify(array1) === JSON.stringify(array2));
+};
+
+
+const takeUntil = function(array, callback) {
+  // callback() returns true when should stop taking
+  // loop the array
+  //  x, and x's index
+  //   if callback(x) returns true then we find our index
+
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (callback(element)) {
+      return array.slice(0, i);
+    }
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+
+// console.log(results1);
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+// console.log(results2);
+
+assertArraysEqual([1, 2, 5, 7, 2], results1);
