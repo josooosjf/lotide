94d77eaad4f8e6d03d1f1ba4a282c79f43e156ea
Josh Brookstone <61817840+joshbrookstone@users.noreply.github.com>
2020-04-06 12:16:10 -0700

    middletest more test cases

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,3 +2,4 @@ const middle = require("../middle");
 const assertArraysEqual = require("../assertArraysEqual");
 
 assertArraysEqual((middle([2,3,4])),(middle([2,3,4])));
+assertArraysEqual((middle([2,3,4,5])),(middle([2,3,4,5])));
